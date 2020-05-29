a26f095004b0787180246b8a5873e3b2594f9cfb
Paulo Henrique Bendazzoli <pbendazzoli@ciandt.com>
2020-05-29 11:47:33 -0300

    usando inbound para passar valores nas propriedades dos componentes

--- AluraPic/src/app/photo/photo.component.ts
+++ AluraPic/src/app/photo/photo.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
-import { templateJitUrl } from '@angular/compiler';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'ap-photo',
     templateUrl: 'photo.component.html'
 })
 export class PhotoComponent {
-    description = 'GuitarraPuzzle';
-    url = 'https://i.pinimg.com/originals/36/ec/bd/36ecbdfd29044348d3cc359a40652829.jpg';
+    //São uma propriedade inbound:
+    // aceitam receber um valor através da sua forma declarativa
+    @Input() description = '';
+    @Input() url = '';
 }
