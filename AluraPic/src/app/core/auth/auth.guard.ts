c60bda681e2769faa08d2d47b20930e2e82b3c60
Paulo Henrique Bendazzoli <pbendazzoli@ciandt.com>
2020-06-05 17:10:04 -0300

    Salvando e protegendo a rota de acao do usuario

---
+++ AluraPic/src/app/core/auth/auth.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { UserSerice } from '../user/user.service';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthGuard implements CanActivate{
+    constructor(private userService:UserSerice,
+                private router: Router){
+
+    }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean>{
+        if(this.userService.isLogged()){
+            this.router.navigate(['user', this.userService.getUserName()])
+            return false;
+        }
+        return true;
+    }    
+}
