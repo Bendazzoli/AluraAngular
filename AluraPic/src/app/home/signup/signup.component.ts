3b4cca2ea61af782487a3711fe3e52f15f1c151d
Paulo Henrique Bendazzoli <pbendazzoli@ciandt.com>
2020-06-08 13:37:13 -0300

    Criando nosso primeiro validator

--- AluraPic/src/app/home/signup/signup.component.ts
+++ AluraPic/src/app/home/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validator';
 
 @Component({
     templateUrl:'./signup.component.html'
@@ -18,7 +19,7 @@ export class SignUpComponent implements OnInit{
                            Validators.minLength(2),
                            Validators.maxLength(40)]],
             userName: ['', [Validators.required,
-                            Validators.pattern(/^[a-z0-9_\-]+$/),
+                            lowerCaseValidator,
                             Validators.minLength(2),
                             Validators.maxLength(30)]],
             password:['', [Validators.required,
