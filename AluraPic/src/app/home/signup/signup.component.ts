0f57142c3feae5c41f950f3b0ae9ecca20f2ab29
Paulo Henrique Bendazzoli <pbendazzoli@ciandt.com>
2020-06-08 14:31:02 -0300

    Resolvendo problema de usuario ja existente

--- AluraPic/src/app/home/signup/signup.component.ts
+++ AluraPic/src/app/home/signup/signup.component.ts
@@ -1,30 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validator';
+import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
 
 @Component({
     templateUrl:'./signup.component.html'
 })
 export class SignUpComponent implements OnInit{
 
-    signForm: FormGroup;
+    signupForm: FormGroup;
     
-    constructor(private formBuilder: FormBuilder){}
+    constructor(private formBuilder: FormBuilder,
+                private userNotTakenValidatorService: UserNotTakenValidatorService){}
 
     ngOnInit(): void {
-        this.signForm = this.formBuilder.group({
-            email:['', [Validators.required,
-                        Validators.email]],
-            fullName:['', [Validators.required,
-                           Validators.minLength(2),
-                           Validators.maxLength(40)]],
-            userName: ['', [Validators.required,
-                            lowerCaseValidator,
+
+        this.signupForm = this.formBuilder.group({
+            email:['', [
+                            Validators.required,
+                            Validators.email
+                        ]
+                    ],
+            fullName:['', [
+                            Validators.required,
                             Validators.minLength(2),
-                            Validators.maxLength(30)]],
-            password:['', [Validators.required,
-                           Validators.minLength(8),
-                           Validators.maxLength(14)]]
+                            Validators.maxLength(40)
+                        ]
+                    ],
+            userName: ['',
+                           [
+                                Validators.required,
+                                lowerCaseValidator,
+                                Validators.minLength(2),
+                                Validators.maxLength(30)
+                           ],
+                           this.userNotTakenValidatorService.checkUserNameTaken()
+                       ],
+            password:['', 
+                            [
+                                Validators.required,
+                                Validators.minLength(8),
+                                Validators.maxLength(14)
+                            ]
+                    ]
         });
     }
 }
