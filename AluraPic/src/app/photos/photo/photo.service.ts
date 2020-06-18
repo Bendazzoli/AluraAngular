c25376afb32ab3af578c8b4a504995f14fa3bd7a
Paulo Henrique Bendazzoli <pbendazzoli@ciandt.com>
2020-06-18 14:12:50 -0300

    buscando comentarios

--- AluraPic/src/app/photos/photo/photo.service.ts
+++ AluraPic/src/app/photos/photo/photo.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { Photo } from "./photo";
+import { PhotoComment } from './photo-comment';
 
 const API = 'http://localhost:3000';
 
@@ -34,8 +35,11 @@ export class PhotoService {
 
     }
 
-    findById(id: string) {
+    findById(photoId: number) {
+        return this.http.get<Photo>(API + '/photos/' + photoId);
+    }
 
-        return this.http.get<Photo>(API + '/photos/' + id);
+    getComments(photoId: number){
+        return this.http.get<PhotoComment[]>(API + '/photos/' + photoId + '/comments');
     }
 }
